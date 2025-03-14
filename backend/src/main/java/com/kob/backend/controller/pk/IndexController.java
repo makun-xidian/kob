3588b75ebd90945a5927c0197ab38d22d64514fb
myname <email@example.com>
2025-03-14 23:54:57 +0800

    项目骨架搭建好了

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,15 +1,22 @@
 package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/pk")
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin
 @RestController
+@RequestMapping("/pk")
 public class IndexController {
     @RequestMapping("/index")
-    public String index(){
-        return "pk/index.html";
+    public Map<String, String> index(){
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "makun");
+        map.put("rating", "8080");
+        return map;
     }
 }
---
+++ web/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>机器人名称：{{ bot_name }}</div>
+    <div>战力：{{ bot_rating }}</div>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default{
+  name: "APP",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/index",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background-image: url("@/assets/background.png");
+  background-size: cover;
+}
+</style>
